582227d09f20ad8c3f8797bfe525f90e6e8b76c3
nadine.loepfe <nadine.loepfe@swirldslabs.com>
2024-10-08 06:23:26 +0200

    gitignore is my friend

--- dist/type.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.ApiBaseUrl = void 0;
-var ApiBaseUrl;
-(function (ApiBaseUrl) {
-    ApiBaseUrl["Production"] = "https://api.fireblocks.io/v1";
-    ApiBaseUrl["Sandbox"] = "https://sandbox-api.fireblocks.io/v1";
-})(ApiBaseUrl || (exports.ApiBaseUrl = ApiBaseUrl = {}));
-//# sourceMappingURL=type.js.map
